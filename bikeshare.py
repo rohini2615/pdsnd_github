8037e7666a571bc96f1f0fa52a0494c22e2b006b
rohini2615 <rohinikataria.rk15@gmail.com>
2020-04-24 20:12:59 -0400

    add installation guidelines

--- bikeshare.py
+++ bikeshare.py
@@ -1,6 +1,8 @@
+### importing libraries
 import time
 import pandas as pd
 import numpy as np
+### importing files
  
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
